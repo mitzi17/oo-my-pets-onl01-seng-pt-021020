70b0c45e2d18a80a82d0361b0b2fbb4839096db1
Mitzi Estrada <m_estrada17@live.com>
2020-03-12 04:18:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,6 +62,6 @@ class Owner
 
   
   def list_pets
-    "I have #{self.dogs.count}dog(s), and #{self.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
